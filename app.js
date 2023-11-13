a4b3d3cce7540c65d45f6b899d70ac3aadd85a36
Eduardo Griesang <eduardogriesang1@gmail.com>
2023-11-13 10:21:01 -0300

    intalled cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express")
+const cors = require("cors")
 const artRoute = require("./routes/artData")
 
 const app = express()
 const port = 8000
 
 app.use(express.json())
+app.use(cors({origin: "*"}))
 app.use('/art', artRoute)
 
 app.listen(port, () => {
