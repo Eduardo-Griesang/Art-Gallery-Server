617f293d99db2f9f6e18747409dba727ad22472e
Eduardo Griesang <eduardogriesang1@gmail.com>
2023-11-15 14:44:06 -0300

    port change

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const cors = require("cors")
 const artRoute = require("./routes/artData")
 
 const app = express()
-const port = 8000
+const port = process.env.PORT
 
 app.use(express.json())
 app.use(cors({origin: "*"}))
 app.use('/art', artRoute)
 
-app.listen(port, () => {
+app.listen(port || 8000, () => {
     console.log(`Listening to port ${port}`)
 })
