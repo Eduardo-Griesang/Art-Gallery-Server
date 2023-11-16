bf8d91d6f547128312771e01632d7e9faa4d2787
Eduardo Griesang <eduardogriesang1@gmail.com>
2023-11-16 09:57:53 -0300

    new art test

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const cors = require("cors")
 const artRoute = require("./routes/artData")
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 8000
 
 app.use(express.json())
 app.use(cors({origin: "*"}))
 app.use('/art', artRoute)
 
-app.listen(port || 8000, () => {
+app.listen(port, () => {
     console.log(`Listening to port ${port}`)
 })
